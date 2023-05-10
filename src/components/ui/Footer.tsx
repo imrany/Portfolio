b445d2333666b3934a56276962831fd1761cad8e
imrany <imranmat254@gmail.com>
2023-05-10 17:46:39 +0300

    improving ui

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -8,14 +8,16 @@ function Footer() {
         <footer className='bg-black text-white py-5 px-2'>
             <div className="flex sm:justify-around max-sm:flex-col max-sm:text-center text-lg my-8" id='links'>
                 <div id='social-links' className='flex flex-col'>
-                    <a className='hover:text-red-500' href='mailto:imranmat254@gmail.com' target='_blank' rel="noopener">imranmat254@gmail.com</a>
-                    <a className='hover:text-green-500' href='https://wa.me/254734720752' target='_blank' rel='noopener'>Whatsapp</a>
-                    <a className='hover:text-blue-500' href="tel:+254734720752" target='_blank' rel='noopener'>Call me</a>
+                    <a className='hover:text-green-500' href='https://wa.me/254734720752' target='_blank' rel='noopener noreferrer'>Whatsapp</a>
+                    <a className='hover:text-red-400' href='https://instagram.com/imrany00' target='_blank' rel="noopener noreferrer">Instagram</a>
+                    <a className='hover:text-purple-400' href='https://twitter.com/matano_imran' target='_blank' rel="noopener noreferrer">Twitter</a>
+                    <a className='hover:text-red-500' href='mailto:imranmat254@gmail.com' target='_blank' rel="noopener noreferrer">Gmail</a>
+                    <a className='hover:text-blue-500' href="tel:+254734720752" target='_blank' rel='noopener noreferrer'>Phone</a>
                 </div>
 
-                <div className="flex flex-col" id='quick-links'>
-                    <a href="https://github.com/imrany" target='_blank' rel='noopener noreferrer' className='hover:text-yellow-500'>My Contributions</a>
-                    <a href='tel:+254734720752' target='_blank' rel='noopener noreferrer' className='hover:text-yellow-500'>Get in touch</a>
+                <div className="flex flex-col max-sm:mt-8" id='quick-links'>
+                    <a href="https://github.com/imrany" target='_blank' rel='noopener noreferrer noreferrer' className='hover:text-yellow-500'>My Contributions</a>
+                    <a href='tel:+254734720752' target='_blank' rel='noopener noreferrer noreferrer' className='hover:text-yellow-500'>Get in touch</a>
                     <a href='#work' className='hover:text-yellow-500'>Work</a>
 
                 </div>
--- src/components/ui/Work.tsx
+++ src/components/ui/Work.tsx
@@ -8,9 +8,9 @@ function Work() {
             <p className='text-2xl text-gray-500 text-center font-semibold mb-10'>Work</p>
             <div className="md:grid md:grid-cols-2 lg:grid-cols-4 max-md:flex max-md:flex-col max-md:items-center">
                 {data&&data.map((i,n)=>(
-                    <div className="md:px-[20px] max-md:w-[60vw] md:w-[40vw] lg:w-[20vw] max-md:mb-4" key={n}>
+                    <div className="md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[20vw] max-md:mb-4" key={n}>
                         <a href={i.imgUrl} target="_blank" rel="noopener noreferrer">
-                            <img src={i.imgUrl} alt={i.title} className='md:w-[600px] max-md:w-[60vw] max-md:h-[45vw] md:h-[200px] rounded-[20px]'/>
+                            <img src={i.imgUrl} alt={i.title} className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[200px] rounded-[20px]'/>
                         </a>
                        <div className="my-6">
                             <p className='text-lg font-semibold mb-4'>{i.title}</p>
