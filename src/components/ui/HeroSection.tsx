9db4d284ee83cf7acdb8477ca50233bc41bf9a94
imrany <imranmat254@gmail.com>
2023-06-02 18:57:03 +0300

    update

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -7,8 +7,8 @@ function HeroSection() {
     return (
         <>
             <div className="flex h-[80vh] max-md:h-[70vh] items-center max-md:justify-center sm:py-6 sm:px-6 border-b max-sm:py-1" id="hero-section">
-                <div id="call-to-action" className='w-[50vw] max-md:w-[90vw]'>
-                    <p className='lg:text-6xl text-3xl'>Explore my software development projects, products and features</p>
+                <div id="call-to-action" className='w-[45vw] max-md:w-[90vw]'>
+                    <p className='lg:text-5xl text-3xl'>Explore my software development projects, products and features</p>
                     <p className='my-6 text-lg w-[35vw] max-md:w-[60vw]'>I'm a fullstack web developer, javascript programmer and a freelancer.</p>
                     <button onClick={email} className='text-base bg-black text-white border-none py-2 px-7 rounded-[50px]'>
                         Hire me
