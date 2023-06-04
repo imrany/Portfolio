8cca9bcfc99bfdcbd90662d5ffcac55957dff8e0
imrany <imranmat254@gmail.com>
2023-06-04 17:11:43 +0300

    added react-slick and slick-carousel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import 'remixicon/fonts/remixicon.css'
 import App from './App.tsx'
 import './index.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
