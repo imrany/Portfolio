d32b72470fb38829a5880958123ffa7356223408
imrany <imranmat254@gmail.com>
2023-06-26 15:51:55 +0300

    update deployed

--- src/components/ui/Skills.tsx
+++ src/components/ui/Skills.tsx
@@ -47,7 +47,7 @@ function Skills() {
     ]
     return (
         <div className="flex flex-col items-center my-10" id="skills">
-            <p className="text-4xl max-md:text-xl">Skills</p>
+            <p className="text-4xl max-md:text-xl">My Skills</p>
             <div className="text-base font-thin lg:w-[70vw] max-md:px-8">
                 <p>
                     I'm experienced in the following areas:
