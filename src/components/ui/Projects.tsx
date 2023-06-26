a433d61b3930e97ae3b62665616f1f7e8745c148
imrany <imranmat254@gmail.com>
2023-06-26 13:50:29 +0300

    errors

--- src/components/ui/Projects.tsx
+++ src/components/ui/Projects.tsx
@@ -4,7 +4,7 @@ import { data } from '../data';
 
 function Projects() {
     const settings = {
-        fade: true,
+        // fade: true,
         speed: 2000,
         autoplaySpeed: 6000,
         infinite: true,
@@ -17,7 +17,7 @@ function Projects() {
         <div className='py-14 flex flex-col items-center' id='projects'>
             <p className="text-4xl max-md:text-xl mb-8">Projects</p>
             <div>
-                <div className="md:grid md:gap-y-12 md:gap-x-8 md:grid-cols-2 lg:grid-cols-3 max-md:flex max-md:flex-col max-md:items-center">
+                <div {...settings} className="hero__slider">
                     {data&&data.map((i,n)=>(
                         <div className="md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:border-b-2 max-md:pb-4" key={n}>
                             <a href={i.imgUrl} target="_blank" rel="noopener noreferrer">
