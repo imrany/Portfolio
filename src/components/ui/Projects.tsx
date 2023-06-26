9f278b6130103e5abdc8fc9ed1a030740add807b
imrany <imranmat254@gmail.com>
2023-06-26 18:03:21 +0300

    added linkedin profile and card

--- src/components/ui/Projects.tsx
+++ src/components/ui/Projects.tsx
@@ -100,8 +100,11 @@ function Projects() {
                             </p>
                         </a>
                     </div>
-                    <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="imranyy" data-version="v1">
-                        <a className="badge-base__link LI-simple-link" href="https://ke.linkedin.com/in/imranmatano?trk=profile-badge" target="_blank" rel="noopener noreferrer">Imran matano</a>
+
+                    <div className="md:px-[20px] max-sm:px-[10px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a className="badge-base__link LI-simple-link" href="https://ke.linkedin.com/in/imranmatano?trk=profile-badge" target="_blank" rel="noopener noreferrer">
+                            <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="imranyy" data-version="v1"></div>
+                        </a>
                     </div>
                 </Slider>
             </div>
