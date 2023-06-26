2d3f4be19d37d0be1e18f48f16a8e476a5f39b79
imrany <imranmat254@gmail.com>
2023-06-26 14:24:34 +0300

    UI updates

--- src/components/ui/Projects.tsx
+++ src/components/ui/Projects.tsx
@@ -1,35 +1,94 @@
 // @flow strict
 import Slider from "react-slick"
-import { data } from '../data';
+import img1 from '../../assets/api.jpg'
+import img2 from '../../assets/blog.jpg'
+import img3 from '../../assets/moon_man.jpg'
 
 function Projects() {
     const settings = {
-        fade: true,
-        speed: 2000,
-        autoplaySpeed: 6000,
+        dots: true,
         infinite: true,
         autoplay: true,
-        slidesToShow: 1,
+        speed: 2000,
+        swipeToSlide: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
         slidesToScroll: 1,
         pauseOnHover: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
     };
     return (
-        <div className='py-14 flex flex-col items-center' id='projects'>
-            <p className="text-4xl max-md:text-xl mb-8">Projects</p>
+        <div className='py-14 px-[10vw]' id='projects'>
+            <p className="text-4xl max-md:text-xl mb-8 text-center">Projects</p>
             <div>
-                <div className="md:grid md:gap-y-12 md:gap-x-8 md:grid-cols-2 lg:grid-cols-3 max-md:flex max-md:flex-col max-md:items-center">
-                    {data&&data.map((i,n)=>(
-                        <div className="md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:border-b-2 max-md:pb-4" key={n}>
-                            <a href={i.imgUrl} target="_blank" rel="noopener noreferrer">
-                                <img src={i.imgUrl} alt={i.title} className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
-                            </a>
-                            <a href={i.url} target="_blank" rel='noopener noreferrer' className="my-6">
-                                <p className='text-lg font-semibold mb-4'>{i.title}</p>
-                                <p className='font-thin'>{i.desc}</p>
-                            </a>
-                        </div>
-                    ))}
-                </div>
+                <Slider {...settings} className="hero__slider">
+                    <div className="max-sm:px-[10px] md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a href={img1} target="_blank" rel="noopener noreferrer">
+                            <img src={img1} alt="Openspace" className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
+                        </a>
+                        <a href="https://developer-openspace-dev.web.app/" target="_blank" rel='noopener noreferrer' className="my-6">
+                            <p className='text-lg font-semibold mb-4'>Openspace</p>
+                            <p className='font-thin'>Get popular campus/university news and blogs with easy, we post about universities in Kenya and around the world</p>
+                        </a>
+                    </div>
+
+                    <div className="max-sm:px-[10px] md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a href={img1} target="_blank" rel="noopener noreferrer">
+                            <img src={img2} alt="Openspace" className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
+                        </a>
+                        <a href="https://developer-openspace-dev.web.app/" target="_blank" rel='noopener noreferrer' className="my-6">
+                            <p className='text-lg font-semibold mb-4'>Campus blogs</p>
+                            <p className='font-thin'>Get popular campus/university news and blogs with easy, we post about universities in Kenya and around the world</p>
+                        </a>
+                    </div>
+
+                    <div className="max-sm:px-[10px] md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a href={img1} target="_blank" rel="noopener noreferrer">
+                            <img src={img3} alt="Openspace" className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
+                        </a>
+                        <a href="https://developer-openspace-dev.web.app/" target="_blank" rel='noopener noreferrer' className="my-6">
+                            <p className='text-lg font-semibold mb-4'>Openspace</p>
+                            <p className='font-thin'>Get popular campus/university news and blogs with easy, we post about universities in Kenya and around the world</p>
+                        </a>
+                    </div>
+
+                    <div className="max-sm:px-[10px] md:px-[20px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a href={img1} target="_blank" rel="noopener noreferrer">
+                            <img src={img1} alt="Openspace" className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
+                        </a>
+                        <a href="https://developer-openspace-dev.web.app/" target="_blank" rel='noopener noreferrer' className="my-6">
+                            <p className='text-lg font-semibold mb-4'>Openspace</p>
+                            <p className='font-thin'>Get popular campus/university news and blogs with easy, we post about universities in Kenya and around the world</p>
+                        </a>
+                    </div>
+
+                    <div className="md:px-[20px] max-sm:px-[10px] max-sm:w-[80vw] max-md:w-[60vw] md:w-[40vw] lg:w-[22vw] max-md:mb-[60px] max-md:pb-4">
+                        <a href={img1} target="_blank" rel="noopener noreferrer">
+                            <img src={img3} alt="Openspace" className='md:w-[600px] max-sm:w-[80vw] max-sm:h-[50vw] max-md:w-[60vw] max-md:h-[45vw] md:h-[170px] rounded-t-[10px]'/>
+                        </a>
+                        <a href="https://developer-openspace-dev.web.app/" target="_blank" rel='noopener noreferrer' className="my-6">
+                            <p className='text-lg font-semibold mb-4'>Openspace</p>
+                            <p className='font-thin'>Get popular campus/university news and blogs with easy, we post about universities in Kenya and around the world</p>
+                        </a>
+                    </div>
+                </Slider>
             </div>
         </div>
     );
