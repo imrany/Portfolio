0bdd81ea015f70547fa29599fdc7a3631d863fee
imrany <imranmat254@gmail.com>
2023-06-26 17:36:43 +0300

    update the autoplay time to 15sec

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -4,7 +4,7 @@ function HeroSection() {
     const settings = {
         fade: true,
         speed: 2000,
-        autoplaySpeed: 6000,
+        autoplaySpeed: 15000,
         infinite: true,
         autoplay: true,
         slidesToShow: 1,
@@ -35,7 +35,7 @@ function HeroSection() {
                 <div id="hero-image2">
                     <div className="flex flex-col h-[80vh] items-center justify-center ">
                         <p className='lg:text-5xl text-3xl'>A Frontend Developer.</p>
-                        <p className='my-6 text-lg text-center'>I've been developing websites using reactjs, svelket and HTML, CSS and Vanillajs.</p>
+                        <p className='my-6 text-lg text-center'>I've been developing websites using reactjs, svelte and HTML, CSS and Vanillajs.</p>
                         <a href="/Resume.pdf" target="_blank" rel="noopenner norefferer" className='text-base border-[1px] border-white py-2 px-7 hover:bg-white hover:text-black'>
                             View Resume
                         </a>
