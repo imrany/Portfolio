0ef62db27a92473057429c24149f8e87de2cd748
imrany <imranmat254@gmail.com>
2023-08-11 20:50:11 +0300

    ui update

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
     const year=date.getFullYear()
     return (
         <footer className='flex justify-center items-center text-sm py-2 lg:mt-20 bg-black text-white'>
-            <span>Copywrite &copy; {year} | All rights reserved</span>
+            <span>Copyright &copy; {year}. All rights reserved</span>
         </footer>
     );
 };
